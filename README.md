c259c78897575a34f62f172ae3a1c93de715305f
Robert Kuntz <roberttkuntz@gmail.com>
2015-02-27 23:24:53 -0500

    adding changes, cleaing up code, need to fix error status

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # mta-service-status
 Ingest data from the MTA's XML feed and render in a human readable format within a web browser. http://mta.info/status/serviceStatus.txt
-#import necessary packages, there may be dependancies
-#set up python virtualenv to compartmentalize package dependancies
-#globally install these
-#apt-get install libxml2-dev libxslt1-dev python-dev
-#install these with the virtualenv active to use the proper python
-#pip install requests
-#pip install lxml
-#pip install Flask
-#pip install gunicorn
-#the later two are included in the virtenv, however the apt-get portions are required
+import necessary packages, there may be dependancies
+set up python virtualenv to compartmentalize package dependancies
+globally install these
+apt-get install libxml2-dev libxslt1-dev python-dev
+install these with the virtualenv active to use the proper python
+pip install requests
+pip install lxml
+pip install Flask
+pip install gunicorn
+the later two are included in the virtenv, however the apt-get portions are required
--- mta_xml/mta_service.py
+++ mta_xml/mta_service.py
@@ -16,21 +16,9 @@ app = Flask(__name__)
 mta_url = 'http://mta.info/status/serviceStatus.txt'
 
 page = requests.get(mta_url) #contains content from xml status
-#print(page.text) # make sure this works
 root = etree.fromstring(page.content) # HtmlElement
 #root = etree.fromstring(page.content) # lxml.etree._Element
 #decoded_page = page.content.decode("utf8") # decode the page content in expected UTF-8
-#root = etree.parse(StringIO(unicode(decoded_page)))
-#print(root.getroot())
-
-#prints all of XML elements recursively
-#for elt in root.getiterator():
-#	print elt.tag
-
-#same as above, but does not us a deprecated function
-#print out via iterator http://infohost.nmt.edu/tcc/help/pubs/pylxml/web/ElementTree-getiterator.html
-#for elt in doc.iter():
-#     print elt.tag
 
 #pull out XML blocks for each MTA service
 #subway
@@ -40,8 +28,6 @@ root = etree.fromstring(page.content) # HtmlElement
 #MetroNorth
 
 #possible statuses include 'PLANNED WORK', 'GOOD SERVICE', 'PLANNED WORK', 
-
-
 
 #list of services ['subway','bus','BT','LIRR','MetroNorth']
 
@@ -53,10 +39,24 @@ for service in services:
 	#print root.xpath('/service/'+service+'/line/text/text()')
 	lines = root.xpath('/service/'+service+'/line/name/text()')
 	status = root.xpath('/service/'+service+'/line/status/text()')
-	details = root.xpath('/service/'+service+'/line/text/text()')
+	#TODO remove gross hack where we assume <text> is always the next sibling to status	
+	
+	detail_text = []
+	for t in root.xpath('/service/'+service+'/line/status/following-sibling::*[1]/text()'):
+		#print t + '\r\r----------------\r\r'
+		if(t):
+			detail_text.append(t)
+			print detail_text[-1]
+		else:
+			detail_text.append('')
+			print detail_text[-1]
+
 	service_data[service] = {}
 	for i in range(len(lines)):
-		service_data[service][lines[i]] = status[i]
+		print str(i)+ '  ' + ('present' if (i in detail_text) else 'absent') +'\r'
+		service_data[service][lines[i]] = status[i],(detail_text[i] if (i in detail_text) else '') 
+		#service_data[service][lines[i]] = status[i],('' if (detail_text[i].isempty()) else detail_text[i]) 
+		#service_data[service][lines[i]] = status[i] #this works
 
 #print(service_data)
 	#stuff {subway: {'1,2,3': 'GOOD STATUS'}}
@@ -70,7 +70,7 @@ def get_service_data():
     return render_template('index.html', service_data=service_data)
 
 if __name__ == '__main__':
-    #app.debug = True
+    app.debug = True
     #app.run()
     app.run(host='104.131.116.94')
 
